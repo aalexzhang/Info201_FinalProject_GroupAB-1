e40fea93686cd73bb225f317d147ce089eb777e9
Chloe Yijun Chen <102646318+ChloeYijunChen@users.noreply.github.com>
2022-05-12 20:04:32 -0700

    Add files via upload

---
+++ Dress Code Data Analysis.R
@@ -0,0 +1,3 @@
+banned_items <- read.csv("https://github.com/the-pudding/data/blob/master/dress-codes/banned_items.csv", stringsAsFactors = FALSE)
+
+clothes_percentages <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/dress-codes/clothes_percentages.csv", stringsAsFactors = FALSE)
