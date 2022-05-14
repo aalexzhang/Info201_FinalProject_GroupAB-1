6cee935a4b137acc833d6d18ed85537d224ec58c
aalexzhang <102832960+aalexzhang@users.noreply.github.com>
2022-05-14 15:32:09 -0700

    Added hues to graph1

--- FinalProject/graph1.R
+++ FinalProject/graph1.R
@@ -9,7 +9,7 @@ top10 <-
   arrange(desc(total)) %>%
   slice(1:10)
   
-ggplot(top10, aes(x = reorder(item, -total), y = total)) +
+ggplot(top10, aes(x = reorder(item, -total), y = total, fill = total)) +
   geom_bar(stat = 'identity') +
   scale_x_discrete(guide = guide_axis(n.dodge=3)) +
   xlab("banned dress item") + 
