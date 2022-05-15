9d9fa49097e857a270db584250e1f89f5a6b83b8
aalexzhang <102832960+aalexzhang@users.noreply.github.com>
2022-05-14 20:48:44 -0700

    Finished third treemap graph

--- FinalProject/graph3.R
+++ FinalProject/graph3.R
@@ -1,4 +1,6 @@
-# Bar Chart comparing banned items between genders
+# Treemap comparing banned items between genders
+install.packages("treemapify")
+library("treemapify")
 
 banned_items_gender_db <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/dress-codes/clothes_percentages.csv", stringsAsFactors = FALSE)
 
@@ -7,3 +9,7 @@ by_gender <-
   group_by(market) %>%
   summarize(total = n())
 
+ggplot(by_gender, aes(area = total, fill = market, label = market)) +
+  geom_treemap() + 
+  geom_treemap_text(fontface = "italic", colour = "white", place = "centre", size = 50) + 
+  ggtitle("Treemap showing proportion between banned male, female, and non-gendered \nclothing items")
